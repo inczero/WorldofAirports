e842821c00d0044415f8a5ef6239c8dbbe20668d
Incze Roland <incze_roland@yahoo.com>
2019-12-03 00:24:00 +0200

    Added data input checker

--- app/src/main/java/com/example/worldofairports/activity/MainActivity.java
+++ app/src/main/java/com/example/worldofairports/activity/MainActivity.java
@@ -8,6 +8,7 @@ import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.worldofairports.R;
@@ -31,6 +32,64 @@ public class MainActivity extends AppCompatActivity {
             setContentView(R.layout.activity_main);
             TextView noSearchResultTextView = findViewById(R.id.main_activity_no_search_result);
             noSearchResultTextView.setVisibility(View.GONE);
+
+            //layout elements
+            final EditText latitudeInputEditText = findViewById(R.id.latitude_input);
+            final EditText longitudeInputEditText = findViewById(R.id.longitude_input);
+            final EditText radiusInputEditText = findViewById(R.id.radius_input);
+            Button searchButton = findViewById(R.id.search_button);
+
+            //on click event implementation on search button
+            searchButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String latitudeInputString = latitudeInputEditText.getText().toString();
+                    String longitudeInputString = longitudeInputEditText.getText().toString();
+                    String radiusInputString = radiusInputEditText.getText().toString();
+
+                    //region input data check
+
+                    if (latitudeInputString.isEmpty()) {
+                        latitudeInputEditText.setError("Please enter latitude in decimal degree!");
+                        latitudeInputEditText.requestFocus();
+                        return;
+                    }
+
+                    //latitude (in decimal degrees) should be in the interval [-90,90]
+                    double latitudeInputValue = Double.parseDouble(latitudeInputString);
+                    if (!(latitudeInputValue >= -90 && latitudeInputValue <= 90)) {
+                        latitudeInputEditText.setError("Latitude value is incorrect!");
+                        latitudeInputEditText.requestFocus();
+                        return;
+                    }
+
+                    if (longitudeInputString.isEmpty()) {
+                        longitudeInputEditText.setError("Please enter longitude in decimal degree!");
+                        longitudeInputEditText.requestFocus();
+                        return;
+                    }
+
+                    //longitude (in decimal degrees) should be in the interval [-180,180]
+                    double longitudeInputValue = Double.parseDouble(longitudeInputString);
+                    if (!(longitudeInputValue >= -90 && longitudeInputValue <= 90)) {
+                        longitudeInputEditText.setError("Longitude value is incorrect!");
+                        longitudeInputEditText.requestFocus();
+                        return;
+                    }
+
+                    if (radiusInputString.isEmpty()) {
+                        radiusInputEditText.setError("Please enter radius in kilometers!");
+                        radiusInputEditText.requestFocus();
+                        return;
+                    }
+
+                    int radiusInputValue = Integer.parseInt(radiusInputString);
+
+                    //endregion
+
+                    
+                }
+            });
 
         }
     }
